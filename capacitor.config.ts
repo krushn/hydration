3754b492df9c5854edee024fe1a336967e87d0a4
krushn <kathrechakrushn@gmail.com>
2023-02-27 18:16:57 +0530

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'hydration',
+  webDir: 'build',
+  bundledWebRuntime: false
+};
+
+export default config;
